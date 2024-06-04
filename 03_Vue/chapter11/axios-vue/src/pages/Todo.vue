2846b4d196f2d36b8fe0e3914fce86433f73430e
Seoyeon Kim <kseoy0607@gmail.com>
2024-06-04 17:49:46 +0900

    2024.06.04 투두리스트 기능 구현

--- 03_Vue/chapter11/axios-vue/src/pages/Todo.vue
+++ 03_Vue/chapter11/axios-vue/src/pages/Todo.vue
@@ -1,7 +1,137 @@
 <template>
   <div>
     <h1>Todo List</h1>
+    <div>
+      <input v-model.trim="todo" type="text" />
+      <button @click="addTodo">추가</button>
+    </div>
+    <br />
+    <ol>
+      <li v-for="todoItem in states.todoList" :key="todoItem.id">
+        <span
+          :style="
+            todoItem.done
+              ? { textDecoration: 'line-through' }
+              : { textDecoration: 'none' }
+          "
+          @click="toggleTodo(todoItem.id)"
+        >
+          {{ todoItem.todo }} {{ todoItem.done ? '(완료)' : '' }}
+        </span>
+        <!-- 수정 모드 -->
+        <div v-if="todoItem.editing">
+          <input v-model.trim="todoEdit" type="text" />
+          <button @click.stop="confirmEditTodo(todoItem.id)">확인</button>
+          <button @click.stop="cancelEditTodo(todoItem.id)">취소</button>
+        </div>
+        &nbsp;
+        <span v-if="!todoItem.editing">
+          <button @click.stop="editTodo(todoItem.id)">수정</button>
+          /
+          <button @click.stop="deleteTodo(todoItem.id)">삭제</button>
+        </span>
+      </li>
+    </ol>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import axios from 'axios';
+import { reactive, ref } from 'vue';
+
+const states = reactive({
+  todoList: [],
+});
+const todo = ref('');
+const todoEdit = ref('');
+
+const BASEURL = '/api/todos';
+
+async function fetchTodoList() {
+  try {
+    const fetchTodoRes = await axios.get(BASEURL);
+
+    states.todoList = fetchTodoRes.data;
+  } catch (e) {
+    alert('TodoList 데이터 통신 Err 발생');
+    console.log(e);
+  }
+}
+
+async function addTodo() {
+  if (todo.value === '') return alert('할 일을 입력해 주세요!');
+
+  try {
+    const newTodo = { todo: todo.value, done: false };
+    const addTodoRes = await axios.post(BASEURL, newTodo);
+
+    if (addTodoRes.status !== 201) return alert('Todo 추가 실패');
+
+    todo.value = '';
+    fetchTodoList();
+  } catch (e) {
+    alert('Todolist 추가 작업 중, Err 발생');
+    console.log(e);
+  }
+}
+
+async function deleteTodo(id) {
+  try {
+    const deleteTodoRes = await axios.delete(`${BASEURL}/${id}`);
+
+    if (deleteTodoRes.status !== 200) return alert('Todo 삭제 실패');
+    fetchTodoList();
+  } catch (e) {
+    alert('Todolist 삭제 작업 중, Err 발생');
+    console.log(e);
+  }
+}
+
+async function toggleTodo(id) {
+  try {
+    const targetTodo = states.todoList.find((todo) => todo.id === id);
+    const payload = { ...targetTodo, done: !targetTodo.done };
+
+    const toggleTodoRes = await axios.put(`${BASEURL}/${id}`, payload);
+
+    if (toggleTodoRes.status !== 200) return alert('Todo toggle 실패');
+    fetchTodoList();
+  } catch (e) {
+    alert('Todolist 토글 작업 중, Err 발생');
+    console.log(e);
+  }
+}
+
+function editTodo(id) {
+  const targetTodo = states.todoList.find((todo) => todo.id === id);
+  states.todoList.map((todo) => (todo.editing = false));
+  if (states.todoList) targetTodo.editing = true;
+}
+
+function cancelEditTodo(id) {
+  const targetTodo = states.todoList.find((todo) => todo.id === id);
+  targetTodo.editing = false;
+  todoEdit.value = '';
+}
+
+async function confirmEditTodo(id) {
+  if (todoEdit.value === '') return alert('수정 내용을 입력하세요!');
+
+  try {
+    const targetTodo = states.todoList.find((todo) => todo.id === id);
+    const payload = { ...targetTodo, todo: todoEdit.value, editing: false };
+
+    const editTodoRes = await axios.put(`${BASEURL}/${id}`, payload);
+
+    if (editTodoRes.status !== 200) return alert('Todo 수정 실패');
+
+    todoEdit.value = '';
+    fetchTodoList();
+  } catch (e) {
+    alert('수정 작업 중, Err 발생');
+    console.log(e);
+  }
+}
+
+fetchTodoList();
+</script>
--- 03_Vue/chapter11/axios-vue/vite.config.js
+++ 03_Vue/chapter11/axios-vue/vite.config.js
@@ -1,16 +1,26 @@
-import { fileURLToPath, URL } from 'node:url'
+import { fileURLToPath, URL } from 'node:url';
 
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import path from 'node:path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-  ],
+  plugins: [vue()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
-  }
-})
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  // 매번 긴 주소를 쓰기 귀찮으므로 /api 라고 설정
+  // /api 라고 주소를 호출 하면 자동으로 http://localhost:3000 호출
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:3000/',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
+    },
+  },
+});
