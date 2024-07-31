e55d02def729ced216b3701869b16169188d94d9
Seoyeon Kim <kseoy0607@gmail.com>
2024-07-31 16:55:06 +0900

    240731 실습ê실습과제 ì업로드

---
+++ 07_Jsp&Sevlet/ex05/src/main/java/org/example/ex05/domain/Member.java
@@ -0,0 +1,2 @@
+package org.example.ex05.domain;public class Member {
+}
---
+++ task/0731/ex05/src/main/java/org/example/ex05/domain/Member.java
@@ -0,0 +1,27 @@
+package org.example.ex05.domain;
+
+public class Member {
+    String name;
+    String userid;
+
+    public Member(String name, String userid) {
+        this.name = name;
+        this.userid = userid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getUserid() {
+        return userid;
+    }
+
+    public void setUserid(String userid) {
+        this.userid = userid;
+    }
+}
